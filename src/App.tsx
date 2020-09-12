f27582c7d30ef0e0f84d6926b451c0b5863258e4
Lauri Nevanperä <lauri.nevanpera@gmail.com>
2020-09-12 19:43:53 +0300

    rename title -> section

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { Title } from "./components/Title";
+import { Section } from "./components/Section";
 import "./index.css";
 import { ShareBar } from "./components/ShareBar";
 
@@ -9,13 +9,13 @@ const AppStyles = styled.div``;
 function App() {
   return (
     <AppStyles className="w-full m-auto max-w-lg flex flex-col items-center px-4">
-      <Title
-        primary="8 Miljardia"
-        secondary="Autoilijat maksavat Suomeen vuosittain kahdeksan miljardia euroa veroina. Katsotaan mistä summa koostuu."
-      ></Title>
-      <Title
-        primary="Verotuotto"
-        secondary={
+      <Section
+        title="8 Miljardia"
+        description="Autoilijat maksavat Suomeen vuosittain kahdeksan miljardia euroa veroina. Katsotaan mistä summa koostuu."
+      ></Section>
+      <Section
+        title="Verotuotto"
+        description={
           <React.Fragment>
             Autoalan tiedotuskeskuksen laskelmien mukaan kahdeksan miljardia
             jakautuu seuraavalla tavalla. <br />
@@ -53,7 +53,8 @@ function App() {
             },
           ]}
         />
-      </Title>
+      </Section>
+      <Section></Section>
     </AppStyles>
   );
 }
--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,17 +1,17 @@
 import * as React from "react";
 
-export const Title = (props: {
-  primary: React.ReactNode;
-  secondary: React.ReactNode;
+export const Section = (props: {
+  title: React.ReactNode;
+  description: React.ReactNode;
   children?: React.ReactNode;
 }) => (
   <div className="h-screen flex flex-col items-center justify-center">
     <div className="sticky top-0">
       <h1 className="font-serif text-5xl md:text-6xl text-teal-500 text-center leading-none">
-        {props.primary}
+        {props.title}
       </h1>
       <p className="text-gray-700 pb-12 pt-12 text-center font-serif">
-        {props.secondary}
+        {props.description}
       </p>
       {props.children}
     </div>
